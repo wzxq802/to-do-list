d9dc2b090bfc1d34b81cbd3be6320264d6496d23
Ulyana <ulyana20011119@gmail.com>
2025-05-30 23:05:55 +0300

    created adding and reading functionality

--- src/components/ToDoApp.vue
+++ src/components/ToDoApp.vue
@@ -2,8 +2,8 @@
   <div class="container">
     <h2 class="text-center mt-5">TO-DO list</h2>
     <div class="d-flex">
-      <input type="text" class="form-control rounded-0" placeholder="Введите текст">
-      <button class="btn btn-warning rounded-0">Отправить</button>
+      <input v-model="task" type="text" class="form-control rounded-0" placeholder="Введите текст">
+      <button v-on:click="submitTask" class="btn btn-warning rounded-0">Отправить</button>
     </div>
     <table class="table table-bordered mt-5">
       <thead>
@@ -15,9 +15,9 @@
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <th> Steal banans from the store</th>
-          <td>Сделать</td>
+        <tr v-for="(task, id) in tasks" :key="id">
+          <th>{{ task.name }}</th>
+          <td>{{ task.status }}</td>
           <td>
             <div class="text-center">
               <span class="fa fa-pen"></span>
@@ -39,6 +39,31 @@ export default {
   name: 'ToDoApp',
   props: {
     msg: String
+  },
+  data () {
+    return {
+      task: '',
+      tasks: [
+      {
+        name: 'Steal banans from the store',
+        status: 'Сделать',
+      },
+      {
+        name: 'Eat 1 kg chocolate in 1 hour',
+        status: 'В работе',
+      }
+    ]
+    }
+  },
+  methods: {
+    submitTask () {
+      if(this.task.length === 0) return;
+
+      this.tasks.push({
+        name: this.task,
+        status: 'Сделать'
+      });
+    }
   }
 }
 </script>
